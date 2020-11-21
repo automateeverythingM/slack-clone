b936c2ad14e38a8e7ede2973b2a46a915a46c227
automateeverythingM <markospavicbg@gmail.com>
2020-11-21 19:58:19 +0100

    firebase env

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/storage";
 import "firebase/database";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAwJqLDgiTEzPVGkRYiDQVh-SxzxEcDk3I",
-    authDomain: "slack-clone-56e13.firebaseapp.com",
-    databaseURL: "https://slack-clone-56e13.firebaseio.com",
-    projectId: "slack-clone-56e13",
-    storageBucket: "slack-clone-56e13.appspot.com",
-    messagingSenderId: "325739575240",
-    appId: "1:325739575240:web:fad4e41709db0ec22072fd",
-    measurementId: "G-KT2BF9HXZS",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
