b836466329e72422c822dd8a17d918d4931bbe45
automateeverythingM <markospavicbg@gmail.com>
2020-11-22 13:42:35 +0100

    spinner on loading

--- src/components/Router/root.js
+++ src/components/Router/root.js
@@ -1,29 +1,28 @@
 import React, { useEffect } from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    useHistory,
-} from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import App from "../App";
 import Login from "../Auth/Login";
 import Register from "../Auth/Register";
 import { auth } from "../../firebase";
 import { withRouter } from "react-router";
+import { connect } from "react-redux";
+import { setUser } from "../../store/actions";
+import { Loader } from "semantic-ui-react";
 
-function Root(props) {
-    console.log("Root -> props", props);
+function Root({ dispatch, isLoading }) {
     const history = useHistory();
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
-            console.log("Root -> history", history);
             if (user) {
+                dispatch(setUser(user));
                 history.push("/");
             }
         });
     }, []);
-    return (
+    return isLoading ? (
+        <Loader size="huge" content="Loading..." active={isLoading} />
+    ) : (
         <Switch>
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
@@ -31,6 +30,11 @@ function Root(props) {
         </Switch>
     );
 }
-const WithRouterSwitch = withRouter(Root);
+const mapStateToProps = (state) => {
+    return {
+        isLoading: state.user.isLoading,
+    };
+};
+const WithRouterSwitch = withRouter(connect(mapStateToProps)(Root));
 
 export default WithRouterSwitch;
